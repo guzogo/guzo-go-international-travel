941e1077b9f0a0c7c7a7426560e11e2f8aa250f0
guzogo <luleabe5@gmail.com>
2026-06-29 15:25:40 +0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,3 +1,39 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+
+import {
+getAuth,
+onAuthStateChanged,
+signOut
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+
+const firebaseConfig = {
+
+apiKey: "AIzaSyBX7MKewr34ciRectfXDJnoZEgF6h-uC3I",
+
+authDomain: "guzo-go-international-travel.firebaseapp.com",
+
+projectId: "guzo-go-international-travel",
+
+storageBucket: "guzo-go-international-travel.firebasestorage.app",
+
+messagingSenderId: "157450888511",
+
+appId: "1:157450888511:web:347bd99e9e97422e0a9f8a"
+
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+onAuthStateChanged(auth,(user)=>{
+
+if(!user){
+
+window.location.href="admin-login.html";
+
+}
+
+});
 import {
   getApplications,
   updateApplicationStatus
@@ -93,3 +129,12 @@ loadApplications();
 });
 
 loadApplications();
+document.getElementById("logoutBtn").addEventListener("click",()=>{
+
+signOut(auth).then(()=>{
+
+window.location.href="admin-login.html";
+
+});
+
+});
