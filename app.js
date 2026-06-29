761cb29caea47419aa9d015f4d4c44e324522ed1
guzogo <luleabe5@gmail.com>
2026-06-29 14:03:12 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,2 @@
 // Guzo Go International Travel & Visa Consultancy
-
-document.addEventListener("DOMContentLoaded", function () {
-
-    const button = document.querySelector(".btn");
-
-    if (button) {
-        button.addEventListener("click", function (e) {
-            e.preventDefault();
-
-            // Go to application page
-            window.location.href = "apply.html";
-        });
-    }
-
-});
+// Future JavaScript code will be added here.
