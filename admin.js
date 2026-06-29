27619efdb9ea8545dce81646bf40b3b7d5d50719
guzogo <luleabe5@gmail.com>
2026-06-29 15:49:54 +0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,29 +1,18 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+import { auth, getApplications, updateApplicationStatus } from "./firebase.js";
 
 import {
-getAuth,
 onAuthStateChanged,
 signOut
 } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
 
-const firebaseConfig = {
+const table = document.getElementById("applicationsTable");
 
-apiKey: "AIzaSyBX7MKewr34ciRectfXDJnoZEgF6h-uC3I",
+const totalApps = document.getElementById("totalApps");
+const approvedApps = document.getElementById("approvedApps");
+const pendingApps = document.getElementById("pendingApps");
+const rejectedApps = document.getElementById("rejectedApps");
 
-authDomain: "guzo-go-international-travel.firebaseapp.com",
-
-projectId: "guzo-go-international-travel",
-
-storageBucket: "guzo-go-international-travel.firebasestorage.app",
-
-messagingSenderId: "157450888511",
-
-appId: "1:157450888511:web:347bd99e9e97422e0a9f8a"
-
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+const logoutBtn = document.getElementById("logoutBtn");
 
 onAuthStateChanged(auth,(user)=>{
 
@@ -34,18 +23,6 @@ window.location.href="admin-login.html";
 }
 
 });
-import {
-  getApplications,
-  updateApplicationStatus
-} from "./firebase.js";
-
-const table = document.getElementById("applicationsTable");
-
-const totalApps = document.getElementById("totalApps");
-const approvedApps = document.getElementById("approvedApps");
-const pendingApps = document.getElementById("pendingApps");
-const rejectedApps = document.getElementById("rejectedApps");
-
 async function loadApplications(){
 
 const applications = await getApplications();
@@ -109,7 +86,9 @@ const id = e.target.dataset.id;
 const success = await updateApplicationStatus(id,"Approved");
 
 if(success){
+
 loadApplications();
+
 }
 
 }
@@ -121,20 +100,21 @@ const id = e.target.dataset.id;
 const success = await updateApplicationStatus(id,"Rejected");
 
 if(success){
+
 loadApplications();
+
 }
 
 }
 
 });
 
+logoutBtn.addEventListener("click", async ()=>{
+
+await signOut(auth);
+
+window.location.href = "admin-login.html";
+
+});
+
 loadApplications();
-document.getElementById("logoutBtn").addEventListener("click",()=>{
-
-signOut(auth).then(()=>{
-
-window.location.href="admin-login.html";
-
-});
-
-});
